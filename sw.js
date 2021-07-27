0e0ea34a3127fd2239da5bad5c6ee00d13e7abf9
demorocket <82033679+demopagerocket@users.noreply.github.com>
2021-07-28 01:39:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,75 @@
-var CACHE_NAME = 'v1';
-var urlsToCache = [
-	'/','manifest.json','favicon-32x32.png','icon-144x144.png','img_1_1_400x400.webp','img_1_1_800x800.webp','img_6_1_1600x400.webp','img_1_15_400x600.webp','index.html','lead.html','gallery.html','article.html','forms.html','grid.html','components.html'
+/*
+ * @license
+ * Your First PWA Codelab (https://g.co/codelabs/pwa)
+ * Copyright 2019 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+'use strict';
+
+// CODELAB: Update cache names any time any of the cached files change.
+const CACHE_NAME = 'v22';
+
+// CODELAB: Add list of files to cache here.
+const FILES_TO_CACHE = [
+		'/','manifest.json','favicon-32x32.png','icon-144x144.png','img_1_1_400x400.webp','img_1_1_800x800.webp','img_6_1_1600x400.webp','img_1_15_400x600.webp','index.html','lead.html','gallery.html','article.html','forms.html','grid.html','components.html'
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );  
+self.addEventListener('install', (evt) => {
+  console.log('[ServiceWorker] Install');
+	// CODELAB: Precache static resources here.
+	evt.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+		  console.log('[ServiceWorker] Pre-caching offline page');
+		  return cache.addAll(FILES_TO_CACHE);
+		})
+	);
+
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('Finally active. Ready to start serving content!');  
+self.addEventListener('activate', (evt) => {
+  console.log('[ServiceWorker] Activate');
+  // CODELAB: Remove previous cached data from disk.
+	evt.waitUntil(
+		caches.keys().then((keyList) => {
+		  return Promise.all(keyList.map((key) => {
+			if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+			  console.log('[ServiceWorker] Removing old cache', key);
+			  return caches.delete(key);
+			}
+		  }));
+		})
+	);
+
+  self.clients.claim();
 });
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+
+self.addEventListener('fetch', (evt) => {
+  console.log('[ServiceWorker] Fetch', evt.request.url);
+	// CODELAB: Add fetch event handler here.
+	if (evt.request.mode !== 'navigate') {
+	  // Not a page navigation, bail.
+	  return;
+	}
+	evt.respondWith(
+		fetch(evt.request)
+			.catch(() => {
+			  return caches.open(CACHE_NAME)
+				  .then((cache) => {
+					return cache.match('index.html');
+				  });
+			})
+	);
+
 });
