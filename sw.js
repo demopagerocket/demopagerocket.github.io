aa84c76ca0055d6811ef2db3c9b00b2bddf871e0
demorocket <82033679+demopagerocket@users.noreply.github.com>
2021-07-28 02:04:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open('v5').then(function (cache) {
       return cache.addAll([
-       	'/','manifest.json','favicon-32x32.png','icon-144x144.png','img_1_1_400x400.webp','img_1_1_800x800.webp','img_6_1_1600x400.webp','img_1_15_400x600.webp','index.html','lead.html','gallery.html','article.html','forms.html','grid.html','components.html',
+       	'/'
         // etc.
       ]);
     }),
