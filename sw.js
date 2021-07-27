c9d45cbf3266a49a41576a7cd01e185b5a575785
demorocket <82033679+demopagerocket@users.noreply.github.com>
2021-07-28 01:18:33 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-self.addEventListener("install", function (event) {
-	event.waitUntil(caches.open("v8").then(function (cache) {
-		return cache.addAll(["/", "manifest.json", "favicon-32x32.png", "icon-144x144.png", "img_1_1_400x400.webp", "img_1_1_800x800.webp", "img_6_1_1600x400.webp", "img_1_15_400x600.webp", "index.html", "lead.html", "gallery.html", "article.html", "forms.html", "grid.html", "components.html"]);
-	}));
-});
-self.addEventListener("fetch", function (event) {
-	event.respondWith(caches.match(event.request).then(function (response) {
-		if (response) {
-			return response;
-		}
-		return fetch(event.request);
-	}));
-});
+self.addEventListener("install",i=>{i.waitUntil(caches.open("v3").then(i=>i.addAll(["/","manifest.json","favicon-32x32.png","icon-144x144.png","index.html","lead.html","gallery.html","article.html","forms.html","grid.html","components.html"]).then(function(){console.log("Success! App is available offline!")})))}),self.addEventListener("fetch", function(event) { event.respondWith( caches.match(event.request) .then(function(response) { if (response) { return response; } return fetch(event.request); } ) );
