32207ddffd51bcb60f44820eec179f5d6e8d5069
demorocket <82033679+demopagerocket@users.noreply.github.com>
2021-08-28 16:56:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 addEventListener('install', event => {
   event.waitUntil(async function() {
     const cache = await caches.open('v1');
-    await cache.add('index.html', 'prices.html', 'infotext.html', 'tabs.html', 'cardsgrid.html', 'contacts.html', 'downloads.html', 'img/favicon-32x32.png', 'img/apple-touch-icon.png', 'img/icon-72x72.png', 'img/icon-96x96.png', 'img/icon-128x128.png', 'img/icon-144x144.png', 'img/icon-152x152.png', 'img/icon-192x192.png', 'img/icon-384x384.png', 'img/icon-512x512.png', 'img/maskable_icon.png');
+    await cache.add('index.html', 'prices.html', 'infotext.html', 'tabs.html', 'cardsgrid.html', 'contacts.html', 'downloads.html');
   }());
 });
 
